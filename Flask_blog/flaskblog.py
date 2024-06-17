0caff14c17fc8786b6d3bd163f9bc1c69d671f10
Darlon Vasata <darlonv@gmail.com>
2024-06-17 10:14:08 -0300

    templates, variables and css included

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Darlon',
+        'title': 'Post 01',
+        'content': 'Conteúdo do Post 01',
+        'date_posted': '02/05/2024'
+    },
+    {
+        'author': 'Vasata',
+        'title': 'Post 02',
+        'content': 'Conteúdo do Post 02',
+        'date_posted': '17/06/2024'
+    }
+    
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1> Home Page<h1>"
+    # return "<h1> Home Page<h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1> About Page<h1>"
+    # return "<h1> About Page<h1>"
+    return render_template('about.html', title='About')
