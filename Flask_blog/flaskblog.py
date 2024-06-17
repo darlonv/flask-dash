9d790b283b4b96d2607e9364fc755ddb40b3c47b
Darlon Vasata <darlonv@gmail.com>
2024-06-17 08:42:04 -0300

    routes home e about

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1> Hello world <h1>"
+@app.route("/home")
+def home():
+    return "<h1> Home Page<h1>"
+
+@app.route("/about")
+def about():
+    return "<h1> About Page<h1>"
